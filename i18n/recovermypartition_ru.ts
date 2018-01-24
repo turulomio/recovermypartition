891fd63cb90159b4cbb8840feb3ceff854b34195
turulomio <turulomio@09598a71-849b-4691-aa78-b3ebc310a349>
2018-01-24 17:59:24 +0000

    Russian translation from google added

--- i18n/recovermypartition_ru.ts
+++ i18n/recovermypartition_ru.ts
@@ -5,143 +5,143 @@
     <message>
         <location filename="../recovermypartition.py" line="320"/>
         <source>You need to pass --partition parameter</source>
-        <translation>Debe añadir el parámetro --partition</translation>
+        <translation>Вам необходимо передать параметр --partition</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="326"/>
         <source>Output directory creating problem. Check it doesn&apos;t exist</source>
-        <translation>Hubo un problema al crear el directorio de salida. Compruebe que no existe</translation>
+        <translation>Проблема с созданием каталога вывода. Проверить, не существует</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="174"/>
         <source>Virtual</source>
-        <translation>Virtual</translation>
+        <translation>виртуальный</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="176"/>
         <source>Deleted</source>
-        <translation>Borrados</translation>
+        <translation>удаленный</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="177"/>
         <source>Files</source>
-        <translation>Ficheros</translation>
+        <translation>файлы</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="310"/>
         <source>Recover files and deleted files from a device or image</source>
-        <translation>Recupera ficheros y ficheros borrados de un dispositivo o imagen</translation>
+        <translation>Восстановление файлов и удаленных файлов с устройства или изображения</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="311"/>
         <source>Don&apos;t recover normal files</source>
-        <translation>No recuperar ficheros normales</translation>
+        <translation>Не восстанавливать нормальные файлы</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="312"/>
         <source>Don&apos;t recover deleted files</source>
-        <translation>No recuperar ficheros borrados</translation>
+        <translation>Не восстанавливать удаленные файлы</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="313"/>
         <source>Device or imagen to analyze</source>
-        <translation>Dispositivo o imagen a analizar</translation>
+        <translation>Устройство или воображение для анализа</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="365"/>
         <source>Output directory</source>
-        <translation>Directorio de salida</translation>
+        <translation>Выходной каталог</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="343"/>
         <source>Generating deleted files list</source>
-        <translation>Generando lista de ficheros borrados</translation>
+        <translation>Создание списка удаленных файлов</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="346"/>
         <source>Generating normal files list</source>
-        <translation>Generando lista de ficheros normales</translation>
+        <translation>Создание списка обычных файлов</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="339"/>
         <source>Generating all files list, including deleted</source>
-        <translation>Generando lista con todos los ficheros, incluyendo los borrados</translation>
+        <translation>Создание списка всех файлов, включая удаленные</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="397"/>
         <source>Recovery took: {0}</source>
-        <translation>La recuperación duró: {0}</translation>
+        <translation>Восстановление: {0}</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="371"/>
         <source>+ Recovering {0} files from partition. Process could take long time.</source>
-        <translation>+ Recuperando {0} ficheros de la partición. El proceso podría durar bastante tiempo.</translation>
+        <translation>+ Восстановление {0} файлов из раздела. Процесс может занять много времени.</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="390"/>
         <source>Recovery ended: {0}
 </source>
-        <translation>La recuperación duró: {0}
+        <translation>Восстановление завершено: {0} 
 </translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="391"/>
         <source>Analyzed files number: {0}
 </source>
-        <translation>Número de ficheros analizados: {0}
+        <translation>Номер анализируемых файлов: {0}
 </translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="392"/>
         <source>Recovery files number: {0}
 </source>
-        <translation>Número de ficheros recuperados: {0}
+        <translation>Номер файла восстановления: {0}
 </translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="247"/>
         <source>I-node is 0</source>
-        <translation>Inodo vale 0</translation>
+        <translation>I-узел равен 0</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="249"/>
         <source>I couldn&apos;t create file</source>
-        <translation>No pude crear el fichero</translation>
+        <translation>Я не могу создать файл</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="251"/>
         <source>I couldn&apos;t create file. Icat error string  greater than 0</source>
-        <translation>No pude crear el fichero. Cadena de error de ical mayor que 0</translation>
+        <translation>Я не мог создать файл. Строка ошибки Icat больше 0</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="253"/>
         <source>Not recovered access time</source>
-        <translation>Hora de acceso no recuperada</translation>
+        <translation>Не восстановленное время доступа</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="255"/>
         <source>Not recovered modification time</source>
-        <translation>Hora de modificación no recuperada</translation>
+        <translation>Не восстановленное время модификации</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="257"/>
         <source>Access and modification time couldn&apos;t be rebuild</source>
-        <translation>Hora de acceso y modificación no puedo ser restituida</translation>
+        <translation>Время доступа и изменения не может быть восстановлено</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="382"/>
         <source>  - Left {0} of {1}. [ Recovered: {2} ]. ETA: {3}                     </source>
-        <translation>  - Quedan {0} de {1}. [ Recuperados: {2} ]. T.Est: {3} </translation>
+        <translation>- Слева {0} из {1} . [Восстановлено: {2} ]. ETA: {3}</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="349"/>
         <source>I couldn&apos;t read the partition</source>
-        <translation>No he podido leer la partición</translation>
+        <translation>Я не мог прочитать раздел</translation>
     </message>
     <message>
         <location filename="../recovermypartition.py" line="364"/>
         <source>Partition or image to analyze: </source>
-        <translation>Partición o imagen a analizar: </translation>
+        <translation>Разделение или изображение для анализа:</translation>
     </message>
     <message>
         <location filename="../Makefile.py" line="74"/>
@@ -151,37 +151,37 @@
     <message>
         <location filename="../Makefile.py" line="71"/>
         <source>Recover normal files and delete files from a partition.</source>
-        <translation type="unfinished"></translation>
+        <translation>Восстановите нормальные файлы и удалите файлы из раздела.</translation>
     </message>
     <message>
         <location filename="../Makefile.py" line="75"/>
         <source>This app has the following parameters.</source>
-        <translation type="unfinished"></translation>
+        <translation>Это приложение имеет следующие параметры.</translation>
     </message>
     <message>
         <location filename="../Makefile.py" line="77"/>
         <source>Scans the net of the interface parameter and prints a list of the detected devices.</source>
-        <translation type="unfinished"></translation>
+        <translation>Сканирует сеть параметра интерфейса и распечатывает список обнаруженных устройств.</translation>
     </message>
     <message>
         <location filename="../Makefile.py" line="78"/>
         <source>If a device is not known, it will be showed in red. Devices in green are trusted devices.</source>
-        <translation type="unfinished"></translation>
+        <translation>Если устройство неизвестно, оно будет показано красным. Устройства в зеленых являются надежными устройствами.</translation>
     </message>
     <message>
         <location filename="../Makefile.py" line="80"/>
         <source>Allows to add a known device from console.</source>
-        <translation type="unfinished"></translation>
+        <translation>Позволяет добавить известное устройство с консоли.</translation>
     </message>
     <message>
         <location filename="../Makefile.py" line="82"/>
         <source>Allows to remove a known device from console.</source>
-        <translation type="unfinished"></translation>
+        <translation>Позволяет удалить известное устройство с консоли.</translation>
     </message>
     <message>
         <location filename="../Makefile.py" line="84"/>
         <source>Shows all known devices in database from console.</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывает все известные устройства в базе данных с консоли.</translation>
     </message>
 </context>
 </TS>
